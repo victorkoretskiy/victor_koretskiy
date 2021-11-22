93c101d7235f7e150abcbc0e47172005dab28785
Viktor Koretskyi <k_viktor@outlook.com>
2021-11-22 10:26:47 +0200

    Update HT_02(2).py

--- HT_02(2).py
+++ HT_02(2).py
@@ -2,7 +2,9 @@
 # який складається із кортежів,
 # і замінить для кожного кортежа останнє значення.
 # https://www.rupython.com/13346-13346.html
-replace_value = input('введите значение для замены  ')
+replace_value = input('введите значение для замены  ').replace("'","")
 list_of_tuples = [(10, 20, 40), (40, 50, 60, 70), (80, 90), (1000,)]
-new_list = [t[:-1] for t in list_of_tuples]
-
+print(replace_value)
+new_list = [list(t[:-1]) + [replace_value] for t in list_of_tuples]
+new_list_of_tuples = [tuple(j) for j in new_list]
+print(new_list_of_tuples)  
