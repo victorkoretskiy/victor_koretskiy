beb045372c572bcb0fd3eeb148240f274583d4a6
victorkoretskiy <94169364+victorkoretskiy@users.noreply.github.com>
2021-12-02 10:54:47 +0000

    Add files via upload

--- 6.py
+++ 6.py
@@ -2,11 +2,19 @@
 # P.S. Повинен вертатись генератор.
 # P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
 def custom_range(start, stop, step = 1):
-    if step > 0:
+    if start != None and stop == None:
+        temp = start
+        start = 0
+        stop = temp
+    if start < stop and step < 0:
+        print([])
+    if start > stop and step >0:
+        print([]) 
+    if step > 0 and start < stop:
         while start !=stop:
             yield start + step
             start += step
-    if step < 0:
+    if step < 0 and start > stop:
          while start !=stop:
             step = abs(step)
             yield start - step
