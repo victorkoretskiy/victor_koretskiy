82bc8189f6a81e5e90bea6cb514dcdef09f6fd20
victorkoretskiy <94169364+victorkoretskiy@users.noreply.github.com>
2021-12-02 13:35:25 +0200

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,7 +1,7 @@
 # 6. Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
 # P.S. Повинен вертатись генератор.
 # P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
-def custom_range(start, stop, step = 1):
+def custom_range(start, stop = None, step = 1):
     if start != None and stop == None:
         temp = start
         start = 0
