45c378acbc268f5e6c0edc0b0746fa9540d7c8f8
victorkoretskiy <94169364+victorkoretskiy@users.noreply.github.com>
2021-12-05 23:47:46 +0200

    Delete 2.py

--- 2.py
+++
@@ -1,4 +0,0 @@
-list1 = input('enter the colours of the first list through the space  ').split()
-list2 = input('enter the colours of the second list through the space  ').split()
-answer = set(list1) - set(list2)
-print(answer)
