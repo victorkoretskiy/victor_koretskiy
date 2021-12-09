f7a0168223ccbd61baa0341e9a012c8fded40160
victorkoretskiy <94169364+victorkoretskiy@users.noreply.github.com>
2021-12-09 17:00:30 +0200

    Delete 6.py

--- 6.py
+++
@@ -1,7 +0,0 @@
-group = list(input('введите группу значений запятую с пробелом  ').split(", "))
-n = input('введите символ для проверки')
-for i in [group]:
-    if n in i:
-        print('True')
-    else:
-        print('False')
