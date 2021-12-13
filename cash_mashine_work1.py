552eeff98861938e740261861e291495b00a5222
Viktor Koretskyi <k_viktor@outlook.com>
2021-12-13 15:51:29 +0200

    Update cash_mashine_work1.py

--- cash_mashine_work1.py
+++ cash_mashine_work1.py
@@ -72,7 +72,8 @@ def display_stock():
             in_stock.append(cell)
     print('The banknotes with a nominals of ', in_stock, ' are available')
     return in_stock
-in_stock = display_stock()
+if func == '3':
+    in_stock = display_stock()
 def check_int():
     """проверяем на целочисленность"""
     while True:
@@ -89,25 +90,28 @@ def check_int():
                 print('Wrong value. The program will be closed)')
             return False
         break
-request = check_int()
+if func == '3':
+    request = check_int()
 def check_positive():
     """проверяем на >0 и кратность самой малой купюре в наличии"""
     if request is not False and request>0 and request%in_stock[-1] == 0:
         return True
     else:
         return False
-check_request = check_positive()
+if func == '3':
+    check_request = check_positive()
 def enough_money():
     """если предыдущие функции ок, то проверяем достаточность денег"""
     if check_request is True and request<=sum(i*j for i,j in case.items()):
         return True
     else:
         return False
-check_done = enough_money()
+if func == '3':
+    check_done = enough_money()
 def cash_machine(request):
     """алгоритм выдачи налички"""
     if check_done == True:
-        cash = {} 
+        cash = {}
         for cell in case:
             if case[cell]<request//cell:
                 cash.update({cell: case[cell]})
@@ -118,13 +122,60 @@ def cash_machine(request):
                 request = request%cell
         stock = []        
         if request>0:
-            for cell in case:
-                if cell>=request and case[cell]>0:
-                    stock.append(cell)
-            print('Try amount which is multiple by ', stock[-1])
+            print('Try amount which is multiple by ', stock[len(stock)-1])
 #        print(request)
 #        print(case)
         return cash
+    else:
+        return None
 if func == '3':
-    print(cash_machine(request))
-
+    cash = cash_machine(request)
+    print('Take your money', cash)
+    #case = { k:int(case_init[k]) - int(cash[k]) for k in case_init if k in cash }
+    #print(case)
+if func == '2':
+    while True:
+        try:
+            with open(name+'_balance.data', 'r') as f:
+                amount = int((f.read()))
+            fill = int(input('enter the amount you want to deposit into the account'))
+            if fill<0:
+                print('Enter the correct value')
+            amount += fill
+            with open(name+'_transactions.json', 'a') as f:
+                f.write(json.dumps('The amount was filled by  '+str(fill)))
+            with open(name+'_balance.data', 'w') as f:
+                f.write(str(amount))
+        except ValueError as err:
+            print('Wrong value. Do you want to try once more? y/n)')
+            answer = str(input())
+            if answer == 'y':
+                continue
+            else:
+                print('Wrong value. The program will be closed)')
+        break                
+if func == '4':
+    print('The cash machine is closed')
+def collector_mode():
+    with open('case.json', 'r') as f:
+        str_case = json.load(f)
+    case = dict((int(k),int(v)) for k,v in str_case.items())
+    print('Entering the collector mode, the initial case is: ', case)
+    slot1 = int(input('enter the new qty of 1000-banknotes'))
+    slot2 = int(input('enter the new qty of 500-banknotes'))
+    slot3 = int(input('enter the new qty of 200-banknotes'))
+    slot4 = int(input('enter the new qty of 100-banknotes'))
+    slot5 = int(input('enter the new qty of 50-banknotes'))
+    slot6 = int(input('enter the new qty of 20-banknotes'))
+    slot7 = int(input('enter the new qty of 10-banknotes'))
+    case = {1000: slot1, 500: slot2, 200: slot3, 100: slot4, 50: slot5, 20:slot6, 10: slot7}
+    with open('case.json', 'w') as f:
+        json.dump(case, f)
+    return case
+if func == '5':
+    col = collector_mode()
+    print(col)
+if func == '1':
+    with open(name+'_balance.data', 'r') as f:
+        amount = int((f.read()))
+    print('your amount is:  ', amount)
