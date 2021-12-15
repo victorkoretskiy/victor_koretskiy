dd9c22e3d588d307655bda2f71d920d5beb19b6c
Viktor Koretskyi <k_viktor@outlook.com>
2021-12-15 22:36:03 +0200

    Update cash_mashine_work1.py

--- cash_mashine_work1.py
+++ cash_mashine_work1.py
@@ -1,22 +1,17 @@
-import json
-with open('users.data', 'r') as f:
-            users = []
-            for line in f:
-                users.append(list(line.rstrip().split(',')))
-with open('case.json', 'r') as f:
-        str_case = json.load(f)
-case = dict((int(k),int(v)) for k,v in str_case.items())
-class RequestExceptionErr(Exception):
-    pass
 class LoginException(Exception):
-    def __init__(self, user_login):
-        self.user_login = user_login
+    pass
+class MenuException(Exception):
+    pass
+import json
 def sign_in():
+    with open('users.data', 'r') as f:
+        users = []
+        for line in f:
+            users.append(list(line.rstrip().split(',')))
     names = dict(users)
     attempts = 3
     for i in range(attempts):
         try:
-            print('collector is Vasya, password: 123456')
             name = str(input('Enter username '))
             password = str(input('Enter password '))
             login = (name, password)
@@ -32,11 +27,7 @@ def sign_in():
             else:
                 print('Login Err after 3 attempts, the program will be closed')
         break
-name = sign_in()
-print(name)
-class MenuException(Exception):
-    pass
-def main_menu():
+def main_menu(name):
     if name is not None:
         print('Greetings, ', name)
         while True:
@@ -62,79 +53,13 @@ def main_menu():
                 else:
                     print('wrong function, try again later')
             break
-func = main_menu()
-print(func)
-def display_stock():
-    """ выводим наличие купюр по номиналу на экран"""
-    # выводим на экран купюры доступные для выдачи
-    in_stock = []
-    for cell in case:
-        if case[cell]>0:
-            in_stock.append(cell)
-    print('The banknotes with a nominals of ', in_stock, ' are available')
-    return in_stock
-if func == '3':
-    in_stock = display_stock()
-def check_int():
-    """проверяем на целочисленность"""
-    while True:
-        try:
-            print('Enter the cash amount which is multiple by ', in_stock[-1])
-            request = int(input())
-            return request
-        except ValueError as err:
-            print('Wrong value. Do you want to try once more? y/n)')
-            answer = str(input())
-            if answer == 'y':
-                continue
-            else:
-                print('Wrong value. The program will be closed)')
-            return False
-        break
-if func == '3':
-    request = check_int()
-def check_positive():
-    """проверяем на >0 и кратность самой малой купюре в наличии"""
-    if request is not False and request>0 and request%in_stock[-1] == 0:
-        return True
-    else:
-        return False
-if func == '3':
-    check_request = check_positive()
-def enough_money():
-    """если предыдущие функции ок, то проверяем достаточность денег"""
-    if check_request is True and request<=sum(i*j for i,j in case.items()):
-        return True
-    else:
-        return False
-if func == '3':
-    check_done = enough_money()
-def cash_machine(request):
-    """алгоритм выдачи налички"""
-    if check_done == True:
-        cash = {}
-        for cell in case:
-            if case[cell]<request//cell:
-                cash.update({cell: case[cell]})
-                request -= cell*case[cell]
-            else:
-                cash.update({cell: request//cell})
-                case[cell]-=request//cell
-                request = request%cell
-        stock = []        
-        if request>0:
-            print('Try amount which is multiple by ', stock[len(stock)-1])
-#        print(request)
-#        print(case)
-        return cash
-    else:
-        return None
-if func == '3':
-    cash = cash_machine(request)
-    print('Take your money', cash)
-    #case = { k:int(case_init[k]) - int(cash[k]) for k in case_init if k in cash }
-    #print(case)
-if func == '2':
+def display_amount(name):
+    """пункт меню 1"""
+    with open(name+'_balance.data', 'r') as f:
+        amount = int((f.read()))
+        print('your amount is:  ', amount) 
+def change_amount(name):
+    """пункт меню 2"""
     while True:
         try:
             with open(name+'_balance.data', 'r') as f:
@@ -153,30 +78,155 @@ if func == '2':
             if answer == 'y':
                 continue
             else:
-                print('Wrong value. The program will be closed)')
+                print('returning to the main menu')
         break                
-if func == '4':
-    print('The cash machine is closed')
+def take_cash(name):
+    """пункт меню 3"""
+    def take_case():
+        """вызываем накопитель банкомата"""
+        with open('case.json', 'r') as f:
+            str_case = json.load(f)
+        case = dict((int(k),int(v)) for k,v in str_case.items())
+        return case
+    case = take_case()
+    def display_stock(case):
+        """выводим наличие купюр по номиналу на экран"""
+        stock = []
+        for cell in case:
+            if case[cell]>0:
+                stock.append(cell)
+        print('The banknotes with a nominals of ', stock, ' are available')
+        return stock
+    stock = display_stock(case) 
+    def check_request():
+        """проверяем запрос выдачи налички на валидность"""
+        while True:
+            try:
+                request = int(input('Please, enter the amount'))
+                with open(name+'_balance.data', 'r') as f:
+                    amount = int((f.read()))
+                if request > sum(i*j for i,j in case.items()):
+                    print('Not anough money. Try another amount? y/n')
+                    answer = str(input())
+                    if answer == 'y':
+                        continue
+                    else:
+                        return None
+                        print('The program is closed')
+                if request <= 0 or request%stock[-1]>0 and request%50>0:
+                    print('Please type the amount which is multiple by',
+                          stock[-1], 'Try another amount? y/n')
+                    answer = str(input())
+                    if answer == 'y':
+                        continue
+                    else:
+                        return None
+                        print('The program is closed')
+                if request > amount:
+                    print('Not enough money on your account. Try another amount? y/n')
+                    answer = str(input())
+                    if answer == 'y':
+                        continue
+                    else:
+                        return None               
+                else:
+                    return request
+            except ValueError as err:
+                print('Please, type correct value. Try once more? y/n)')
+                answer = str(input())
+                if answer == 'y':
+                    continue
+                else:
+                    return None
+                    print('The program is closed')       
+            break
+    request = check_request()
+    def cash_machine(request):
+        """алгоритм выдачи налички"""
+        case_init = case
+        cash = {}
+        request1 = request
+        if (request%100)%20 == 0 and case.get(10) == 0:
+            case.pop(50)
+        for cell in case:
+            if case[cell]<request//cell:
+                cash.update({cell: case[cell]})
+                request -= cell*case[cell]                
+            else:
+                cash.update({cell: request//cell})
+                case[cell]-=request//cell
+                request = request%cell
+        if request!=0:
+            print('Try another amount, recomended: ', request1 - request)
+        else:
+            case_init = { k:int(case_init[k]) - int(cash[k]) for k in case_init if k in cash }
+            cash_final = {}
+            for cell in cash:
+                if cash[cell] != 0:
+                    cash_final.update({cell: cash[cell]})
+            print('Take your money', cash_final)           
+            with open(name+'_balance.data', 'r') as f:
+                init_amount = int((f.read()))
+            amount = init_amount - sum(i*j for i,j in cash_final.items())
+            with open(name+'_balance.data', 'w') as f:
+                f.write(str(amount))          
+            with open('case.json', 'w') as f:
+                json.dump(case, f)
+        return cash_final
+    return cash_machine(request)   
 def collector_mode():
+    """пункт меню 5"""
     with open('case.json', 'r') as f:
         str_case = json.load(f)
     case = dict((int(k),int(v)) for k,v in str_case.items())
-    print('Entering the collector mode, the initial case is: ', case)
-    slot1 = int(input('enter the new qty of 1000-banknotes'))
-    slot2 = int(input('enter the new qty of 500-banknotes'))
-    slot3 = int(input('enter the new qty of 200-banknotes'))
-    slot4 = int(input('enter the new qty of 100-banknotes'))
-    slot5 = int(input('enter the new qty of 50-banknotes'))
-    slot6 = int(input('enter the new qty of 20-banknotes'))
-    slot7 = int(input('enter the new qty of 10-banknotes'))
-    case = {1000: slot1, 500: slot2, 200: slot3, 100: slot4, 50: slot5, 20:slot6, 10: slot7}
+    print('Entering the collector mode, the initial case is:')
+    print(case)
+    for cell in case:
+        print('To load the new case, please define quantities of banknotes in cells')
+        print('Enter the qty of ', cell)
+        case[cell] = input()
     with open('case.json', 'w') as f:
         json.dump(case, f)
-    return case
-if func == '5':
-    col = collector_mode()
-    print(col)
-if func == '1':
-    with open(name+'_balance.data', 'r') as f:
-        amount = int((f.read()))
-    print('your amount is:  ', amount)
+    print('the case has been loaded')
+    print(case)
+def start():
+    name = sign_in()
+    while True:
+        func = main_menu(name)
+        if func =='1':
+            display_amount(name)
+            answer = input('take another action(y - main menu, any key - exit)')
+            if answer == 'y':
+                continue
+            else:
+                break
+        if func == '2':
+            change_amount(name)
+            answer = input('take another action(y - main menu, any key - exit)')
+            if answer == 'y':
+                continue
+            else:
+                break
+        if func == '3':
+            take_cash(name)
+            answer = input('take another action(y - main menu, any key - exit)')
+            if answer == 'y':
+                continue
+            else:
+                break
+        if func == '4':
+            break
+        if func == '5':
+            collector_mode()
+            answer = input('take another action(y - main menu, any key - exit)')
+            if answer == 'y':
+                continue
+            else:
+                break
+        else:
+            continue
+print(start())
+
+
+        
+    
